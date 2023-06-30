34b8a402d6d529e507b010c31ef7059847d8f31c
Replit user <>
2023-06-30 01:39:18 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -70,10 +70,16 @@ HashMap * createMap(long capacity) {
 }
 
 
-void eraseMap(HashMap * map,  char * key) {    
+void eraseMap(HashMap * map, char * key) {
+    Pair * pair = searchMap(map, key);
 
-
+    if (pair != NULL) {
+        free(pair->key);
+        pair->key = NULL;
+        map->size--;
+    }
 }
+
 
 Pair * searchMap(HashMap * map, char * key) {
     long position = hash(key, map->capacity);
